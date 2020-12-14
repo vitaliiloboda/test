6b822a835247f97c906bec5a76ddbe2e486c5fe5
vitalii.loboda <vitalii.loboda@gmail.com>
2020-12-14 22:41:53 +0200

    task_1.py updated

---
+++ task_1.py
@@ -0,0 +1,24 @@
+name = 'Alex'
+age = 35
+ukrainian = True
+height = 180.20
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(ukrainian)
+print(type(ukrainian))
+
+print(height)
+print(type(height))
+
+print(f'Имя - {name}, возраст - {age}, рост - {height}, украинец - {ukrainian}')
+
+number = int(input('Введите любое число: '))
+phrase = input('Введите первую строчку вашего любимого стиха: ')
+
+print(f'Ваше число: {number}')
+print(f'Любимая строчка: {phrase}')
